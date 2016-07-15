bccf6a262d79ac07f02141a841d3b4b8bf469a57
wangzhongshan <1650043271@qq.com>
2016-07-15 16:42:44 +0800

    add

---
+++ src/main/java/Gen/MyCommentGenerator.java
@@ -0,0 +1,45 @@
+package Gen;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.mybatis.generator.internal.util.StringUtility;
+
+/**
+ * @author wangzhongshan
+ * @created 2016-07-12 11:23
+ */
+public class MyCommentGenerator extends DefaultCommentGenerator{
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        topLevelClass.addJavaDocLine("/**");
+        String remarks = introspectedTable.getRemarks();
+        if (StringUtility.stringHasValue(remarks)) {
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            for (String remarkLine : remarkLines) {
+                topLevelClass.addJavaDocLine(" * " + remarkLine);
+            }
+        }
+        topLevelClass.addJavaDocLine(" * "+introspectedTable.getFullyQualifiedTable());
+        topLevelClass.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        if (introspectedColumn.getRemarks() != null && !introspectedColumn.getRemarks().equals("")) {
+            field.addJavaDocLine("/** "+ introspectedColumn.getRemarks()+" */");
+        }
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+    }
+
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+    }
+}
